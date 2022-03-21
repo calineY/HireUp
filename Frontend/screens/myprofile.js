beb186c0542dbbebe7db5f2257af67d0c8087621
Caline <calineyammine123@gmail.com>
2022-03-21 23:23:15 +0200

    Added my profile screen

---
+++ Frontend/screens/myprofile.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyProfile = () => {
+  return (
+    <View>
+      <Text>myprofile</Text>
+    </View>
+  )
+}
+
+export default MyProfile
