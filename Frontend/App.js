3101793bbf9beffdd1f90e7058d1593a54732f02
Caline <calineyammine123@gmail.com>
2022-03-21 18:07:17 +0200

    Added navigation for existing screens

--- Frontend/App.js
+++ Frontend/App.js
@@ -3,9 +3,20 @@ import Login from './screens/login'
 import Signup from './screens/signup'
 import Home from './screens/home'
 import Freelancers from './screens/freelancers'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import MyTabs from './components/Navbar';
+
+const LoginStack=createNativeStackNavigator();
 
 export default function App() {
   return (
-     <Freelancers/>
+    <NavigationContainer>
+      <LoginStack.Navigator screenOptions={{headerShown:false}}>
+        <LoginStack.Screen name='Login' component={Login}/>
+        <LoginStack.Screen name='Register' component={Signup}/>
+        <LoginStack.Screen name='inside'component={MyTabs}/>
+     </LoginStack.Navigator>
+     </NavigationContainer>
   );
 }
