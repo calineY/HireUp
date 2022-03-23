d0909d185d4eea07cb53e1a02d33ab77674b0a8a
Caline <calineyammine123@gmail.com>
2022-03-23 12:03:54 +0200

    Refactored App.js file

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,22 +1,9 @@
 
-import Login from './screens/login'
-import Signup from './screens/signup'
-import Home from './screens/home'
-import Freelancers from './screens/freelancers'
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import MyTabs from './components/Navbar';
+import StackSwitcher from './Navigation/StackSwitcher';
 
-const LoginStack=createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <LoginStack.Navigator screenOptions={{headerShown:false}}>
-        <LoginStack.Screen name='Login' component={Login}/>
-        <LoginStack.Screen name='Register' component={Signup}/>
-        <LoginStack.Screen name='inside'component={MyTabs}/>
-     </LoginStack.Navigator>
-     </NavigationContainer>
+    <StackSwitcher/>
   );
 }
