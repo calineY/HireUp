00e26776c85be371f430f385af0c346069fb7a26
Caline <calineyammine123@gmail.com>
2022-03-26 00:47:15 +0200

    Linked login api

--- Frontend/navigation/stackSwitcher.js
+++ Frontend/navigation/stackSwitcher.js
@@ -14,7 +14,7 @@ const StackSwitcher = () => {
 
   return (
     <NavigationContainer>
-        {false?<LoginStack/>:<TabNavigation/>}
+        {true?<LoginStack/>:<TabNavigation/>}
      </NavigationContainer>
   )
 }
--- Frontend/screens/login.js
+++ Frontend/screens/login.js
@@ -4,19 +4,53 @@ import { globalStyles } from '../styles/global';
 import hireup from '../assets/hireup.png';
 import Input from '../components/input';
 import Button from '../components/buttonLarge'
+import {useState} from 'react';
+import axios from 'axios';
 
 
 export default function Login({navigation}){
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+      });
+    
+      const handleEmail = (value) => {
+        setData({
+          ...data,
+          email: value,
+        });
+      };
+      const handlePassword = (value) => {
+        setData({
+          ...data,
+          password: value,
+        });
+      };
+
+      const loginFetch = async () => {
+        const url = "http://192.168.1.9:8000/api/auth/login";
+    
+        try {
+          const response = await axios.post(url, data);
+          const dataFetched = await response.data;
+          console.warn(dataFetched);
+        } catch (error) {
+          console.warn("error");
+        }
+      };
+
     return(
+       
         <View style={globalStyles.container}>
+             
             <Image style={globalStyles.img} source={hireup}/>
             <View style={globalStyles.container2}>
                 <Text style={globalStyles.label}> Email</Text>
-                <Input placeholder='Jhon@mail.com'/>
+                <Input placeholder='Jhon@mail.com' value={data.email} setValue={handleEmail}/>
                 <Text style={globalStyles.label}>Password</Text>
-                <Input placeholder='Your password' secureTextEntry={true}/>
+                <Input placeholder='Your password' secureTextEntry={true} value={data.password} setValue={handlePassword}/>
                 <View style={globalStyles.margin}></View>
-                <Button text='Login' onPress={()=> console.warn("log in")}/>
+                <Button text='Login' onPress={loginFetch}/>
                 <TouchableOpacity onPress={()=> navigation.navigate("Register")}>
                 <View><Text style={globalStyles.loginSignup}>Don't have an account? Sign Up</Text></View>
                 </TouchableOpacity>
