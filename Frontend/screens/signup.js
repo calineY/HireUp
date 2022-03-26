4c6dcfed8dd44cf315069339e753d5114862deb8
Caline <calineyammine123@gmail.com>
2022-03-26 03:46:55 +0200

    Linked sign up api

--- Frontend/screens/signup.js
+++ Frontend/screens/signup.js
@@ -6,6 +6,8 @@ import Input from '../components/input';
 import Button from '../components/buttonLarge'
 import { useState } from 'react';
 import PhoneInput from "react-native-phone-number-input";
+import axios from 'axios';
+
 
 
 import MapView, { Marker } from 'react-native-maps';
@@ -13,16 +15,74 @@ import MapView, { Marker } from 'react-native-maps';
 
 
 export default function Signup({navigation}){
+  
+
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+    phone_number: "",
+    latitude: "",
+    longitude:"",
+  });
+
+  const handleName = (value) => {setData({...data,name: value,});
+  };
+  const handleEmail = (value) => {
+    setData({
+      ...data,
+      email: value,
+    });
+  };
+  const handlePassword = (value) => {
+    setData({
+      ...data,
+      password: value,
+    });
+  };
+  const handlePasswordConfirmation = (value) => {
+    setData({
+      ...data,
+      password_confirmation: value,
+    });
+  };
+  const handlePhoneNumber = (value) => {
+    setData({
+      ...data,
+      phone_number: value,
+    });
+  };
+  const handleLocation = (lat,long) => {
+    setData({
+      ...data,
+      latitude: lat,
+      longitude: long,
+    });
+  };
+
+  const registerFetch = async () => {
+    const url = "http://192.168.1.9:8000/api/auth/register";
+
+    try {
+      const response = await axios.post(url, data);
+      const dataFetched = await response.data;
+      console.warn(dataFetched);
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
     const [pin, setPin] = useState({
-        latitude: 33.890536626710244,
-        longitude: 35.489303601542964,
+      latitude: 33.89020967953036,
+      longitude: 35.76620947569609,
       });
-      const [region, setRegion] = useState({
-        latitude: 33.890536626710244,
-        longitude: 35.489303601542964,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      });
+      // const [region, setRegion] = useState({
+      //   latitude: 33.890536626710244,
+      //   longitude: 35.489303601542964,
+      //   latitudeDelta: 0.0922,
+      //   longitudeDelta: 0.0421,
+      // });
     return(
         <View style={globalStyles.container}>
 
@@ -30,17 +90,17 @@ export default function Signup({navigation}){
             <View style={globalStyles.container3}>
             <ScrollView >
                 <Text style={globalStyles.label}> Full Name</Text>
-                <Input placeholder='Jhon Doe'/>
+                <Input placeholder='Jhon Doe' value={data.name} setValue={handleName}/>
                 <Text style={globalStyles.label}> Email</Text>
-                <Input placeholder='Jhon@mail.com'/>
+                <Input placeholder='Jhon@mail.com' value={data.email} setValue={handleEmail}/>
                 <Text style={globalStyles.label}>Password</Text>
-                <Input placeholder='Your password' secureTextEntry={true}/>
+                <Input placeholder='Your password' secureTextEntry={true} value={data.password} setValue={handlePassword}/>
                 <Text style={globalStyles.label}>Confirm Password</Text>
-                <Input placeholder='Confirm password' secureTextEntry={true}/>
+                <Input placeholder='Confirm password' secureTextEntry={true} value={data.password_confirmation} setValue={handlePasswordConfirmation}/>
 
                 <Text style={globalStyles.label}>Phone Number</Text>
                 <View style={{alignItems:'center'}}>
-                  <PhoneInput />
+                  <PhoneInput defaultCode='LB' onChangeFormattedText={(text) => {handlePhoneNumber(text); console.log(data.phone_number)}}/>
                 </View>
                 <Text style={globalStyles.label}>Location</Text>
                 <View style={styles.container}>             
@@ -54,25 +114,26 @@ export default function Signup({navigation}){
                     provider="google">
                     <Marker coordinate={pin}
                         draggable={true}
-                        onDragStart={(e) => {
-                            console.log("Drag start", e.nativeEvent.coordinate);
-                          }}
                         onDragEnd={(e) => {
                           console.log("Drag end", e.nativeEvent.coordinate),
                           setPin({
                               latitude: e.nativeEvent.coordinate.latitude,
                               longitude: e.nativeEvent.coordinate.longitude,
                             });
-                            setRegion({
-                              latitude: e.nativeEvent.coordinate.latitude,
-                              longitude: e.nativeEvent.coordinate.longitude,
-                            });
+                          handleLocation( e.nativeEvent.coordinate.latitude,
+                            e.nativeEvent.coordinate.longitude);
+                            // setRegion({
+                            //   latitude: e.nativeEvent.coordinate.latitude,
+                            //   longitude: e.nativeEvent.coordinate.longitude,
+                            //   latitudeDelta: 0.0922,
+                            //   longitudeDelta: 0.0421,
+                            // });
                         }}
                         />
                 </MapView>
 
             </View>
-                <Button text='SIGN UP'/>
+                <Button text='SIGN UP' onPress={registerFetch}/>
                 <TouchableOpacity onPress={()=> navigation.navigate("Login")}>
                 <View><Text style={globalStyles.loginSignup}>Already have an account? Login</Text></View>
                 </TouchableOpacity>
