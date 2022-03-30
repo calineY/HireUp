f12a8bc71536ffc2600799e078bc013d39c39f5b
Caline <calineyammine123@gmail.com>
2022-03-30 04:05:21 +0300

    Linked apis to fill my profile screen

--- Frontend/screens/myprofile.js
+++ Frontend/screens/myprofile.js
@@ -1,131 +1,146 @@
 
-import { View, Text,Linking,StyleSheet } from 'react-native'
+import { View, Text,StyleSheet,Image } from 'react-native'
 import React from 'react'
-import { FontAwesome5 } from '@expo/vector-icons';
 import { Fontisto } from '@expo/vector-icons'; 
 import { FontAwesome } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
 import SmallButton from '../components/button'
 import { globalStyles } from '../styles/global';
 import { ScrollView } from 'react-native-gesture-handler';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { BottomSheet } from 'react-native-btr';
 import Header from '../components/header'
+import { useContext } from 'react';
+import { userContext } from '../userContext';
+import axios from 'axios';
+import { myprofileStyles } from '../styles/myprofileStyles';
+import Review from '../components/review';
+
 
 
 
 const MyProfile = () => {
-    const redirectToWhatsapp = () =>{
-        Linking.openURL('whatsapp://send?text=Hello I found you on HireUp!&phone=+96171767010');
-    }
-    const [visible, setVisible] = useState(false);
-
+  const { authUser, setAuthUser } = useContext(userContext);
+  const [visible, setVisible] = useState(false);
+  const token=authUser.access_token;
   const toggleBottomNavigationView = () => {
     //Toggling the visibility state of the bottom sheet
     setVisible(!visible);
   };
-    const [freelancers,setFreelancers]=useState([
-        {
-            
-            name: "John Doe",
-            picture_url: "user.png",
-            id: 1,
-            from_user_id: 2,
-            to_user_id: 3,
-            rating: 5,
-            review: "Very professional",
-            created_at: "2022-03-13T19:54:49.000000Z",
-            updated_at: "2022-03-13T19:54:49.000000Z"
-          },
-          {
-            name: "Karim Khalifeh",
-            picture_url: "user.png",
-            id: 2,
-            from_user_id: 2,
-            to_user_id: 3,
-            rating: 4,
-            review: "Friendly",
-            created_at: "2022-03-13T19:54:49.000000Z",
-            updated_at: "2022-03-13T19:54:49.000000Z"
-          },
-          {
-            name: "Jane Doe",
-            picture_url: "user.png",
-            id: 3,
-            from_user_id: 2,
-            to_user_id: 3,
-            rating: 4,
-            review: "Great",
-            created_at: "2022-03-13T19:54:49.000000Z",
-            updated_at: "2022-03-13T19:54:49.000000Z"
-          },
-          {
-            name: "Jane Doe",
-            picture_url: "user.png",
-            id: 4,
-            from_user_id: 2,
-            to_user_id: 3,
-            rating: 4,
-            review: "Great",
-            created_at: "2022-03-13T19:54:49.000000Z",
-            updated_at: "2022-03-13T19:54:49.000000Z"
-          },
 
-        ]);
-  return (
-    <View style={{marginBottom:70}}>
-    <View>
-      <Header title='My profile'/>
-    </View>
-      <ScrollView>
-      <View style={{alignContent:"center",flex:1, padding:30}}>
-        <View style={{flexDirection:"row"}}>
-            <View style={{flex:0.4}}>
-                <FontAwesome5 name="user-circle" size={100} color="black"/>
-            </View>
-            <View style={{ flex:0.6 }}>
-                <Text style={{ fontSize: 28, fontWeight: "bold" }}>
-                    John Doe
-                </Text>
-                <Text style={{ fontSize: 22 }}>
-                    Interior designer
-                </Text>
-                <View style={{flexDirection:"row", flex:0.33}}>
+function calculateRating(reviews){
+  let sum =0;
+  let ratings=0;
+  reviews.forEach(element => {
+    ratings+=element.rating;
+    sum++;
+  });
+  return ratings/sum+'/5'+'('+sum+')';
+}
+
+  const user_id=authUser.user.id;
+  const getWorkProfile = async () => {
+      const url = `http://192.168.1.231:8000/api/user/getprofile?user_id=${user_id}`;
+      try {
+        const response = await axios.get(url,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        const dataFetched =response.data;
+        setWorkProfile(dataFetched);
+        console.log(dataFetched);
+      } catch (error) {
+        console.warn(error);
+      }
+    };
+    const getReviews = async () => {
+      const url = `http://192.168.1.231:8000/api/user/reviews?user_id=${user_id}`;
+      try {
+        const response = await axios.get(url,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        const dataFetched =response.data;
+        setReviews(dataFetched);
+        console.log(dataFetched)
+      } catch (error) {
+        console.warn(error);
+      }
+    };
+    const [reviews,setReviews]=useState();
+    const [workProfile,setWorkProfile]=useState();
+
+    useEffect(() => {
+      getWorkProfile();
+      getReviews();
+  },[] );
+  return (<View>
+    {workProfile && reviews?
+<View style={myprofileStyles.mainView}>
+  <View>
+    <Header title='My profile'/>
+  </View>
+  <ScrollView>
+  <View style={myprofileStyles.innerView}>
+    <View style={myprofileStyles.headerView}>
+        <View style={{flex:0.4}}>
+          <View  style={myprofileStyles.imageView}>
+            <Image style={myprofileStyles.image} source={{uri:`http://192.168.1.231:8000/${authUser.user.picture_url}`}}/>
+          </View>
+        </View>
+        <View style={{ flex:0.6 }}>
+            <Text style={myprofileStyles.name}>
+                {authUser.user.name}
+            </Text>
+            {/* if user has no work profile */}
+            {workProfile && workProfile.job[0] ?
+            <View>
+              <Text style={{ fontSize: 22 }}>
+                {workProfile.job[0].title}
+              </Text>
+              <View style={{flexDirection:"row", flex:0.33}}>
                 <FontAwesome name="dollar" size={24} color="orange" />
-                <Text style={{marginLeft:3}}>20/hour</Text>
+                <Text style={{marginLeft:3}}>{workProfile && workProfile.job[0].rate_per_hour}/hour</Text>
+              </View>
             </View>
-            </View>
+            :<View><Text>No position entered</Text></View>}
         </View>
-        <View style={{justifyContent:'center',}}>
-        </View>
-        <View style={{flexDirection:"row", justifyContent:'center',margin:15}}>
-            <SmallButton text="Available" color="#33C47E"/>
-            <SmallButton text="Edit Profile" color="#7C9BC9"  />
-        </View>
-        <Text style={{ fontSize: 19, fontWeight:'bold' }}>Bio</Text>
-        <Text style={globalStyles.bio}>Lorem ipsum dolor sit amet,  et dolore magna aliqua. Ut enim ad minim veniamlabore et dolore magna aliqua. Ut enim ad minim veniamlabore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Text>
+    </View>
+    
+    {workProfile && workProfile.job[0] ?
+    <View style={{flexDirection:"row", justifyContent:'center',margin:15}}>
+        <SmallButton text="Available" color="#33C47E"/>
+        <SmallButton text="Edit Profile" color="#7C9BC9"  />
+    </View>
+    :<View style={{flexDirection:"row", justifyContent:'center',margin:15}}>
+        <SmallButton text="Add work profile" color="#33C47E"/>
+    </View>}
+    {workProfile && workProfile.job[0] ?
+    <View>
+      <Text style={{ fontSize: 19, fontWeight:'bold' }}>Bio</Text>
+      <Text style={globalStyles.bio}>{workProfile && workProfile.job[0].bio} </Text>
+    </View>
+    :<Text style={myprofileStyles.noProfile}>No work profile</Text>}
+    
+    {reviews && !reviews.reviews.length ==0 ?
+    <View>
+      <View style={{flexDirection:'row'}}>
+        <Text style={{ fontSize: 19, fontWeight:'bold',flex:1}}>Reviews</Text>
         <View style={{flexDirection:'row'}}>
-            <Text style={{ fontSize: 19, fontWeight:'bold',flex:1}}>Reviews</Text>
-            <View style={{flexDirection:'row'}}>
-            <Fontisto name="star" size={22} color="#33C47E" />
-                <Text style={{marginLeft:3}}>5/5(2)</Text>
-            </View>
+        <Fontisto name="star" size={22} color="#33C47E" />
+            <Text style={{marginLeft:3}}>{calculateRating(reviews.reviews)}</Text>
         </View>
-        {freelancers.map((item)=>
-            <View key={item.id}>
-            <View style={{backgroundColor:'#fff',width:360,borderRadius: 20,marginBottom:10,alignSelf:'center',padding:15,textAlignVertical:'center',}}>
-                <View style={{flexDirection:'row',marginBottom:3 }}>
-                    <Text style={{ fontSize: 16, fontWeight: "bold", flex:0.8 }}>{item.name}</Text>
-                    <View style={{flexDirection:"row", flex:0.2}}>
-                        <Fontisto name="star" size={22} color="#33C47E" />
-                        <Text style={{marginLeft:3}}>{item.rating}/5</Text>
-                    </View>
-                </View>
-                <View style={{marginTop:3,marginStart:3,flexDirection:"row"}}>
-                    <Text>{item.review}</Text>
-                </View>
-            </View>
-            </View>)}
+    </View>
+    {reviews.reviews.map((item)=>
+        <View key={item.id}>
+          <Review name={item.name} rating={item.rating} review={item.review}/>
+        </View>)}
+    </View>
+    :<View>
+      <View style={{flexDirection:'row'}}>
+        <Text style={{ fontSize: 19, fontWeight:'bold',flex:1}}>Reviews</Text>
+      </View>
+      <Text style={{alignSelf:'center',marginTop:40}}>No reviews</Text>
+    </View>}
  </View>
  <BottomSheet
           visible={visible}
@@ -140,10 +155,11 @@ const MyProfile = () => {
             </View>
         </BottomSheet>
  </ScrollView>
+ </View>:<Text style={{alignSelf:'center',marginVertical:300}}>Loading...</Text>}
  </View>
-
   )
 }
+
 
 export default MyProfile
 
