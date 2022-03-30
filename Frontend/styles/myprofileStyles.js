606218e93c787e47016d2ca3f6bab40d7c7700cd
Caline <calineyammine123@gmail.com>
2022-03-30 04:03:21 +0300

    Added style sheet for my profile

---
+++ Frontend/styles/myprofileStyles.js
@@ -0,0 +1,40 @@
+import { StyleSheet} from 'react-native';
+
+export const myprofileStyles = StyleSheet.create({
+    mainView:{
+        marginBottom:70
+    },
+    innerView:{
+        alignContent:"center",
+        flex:1,
+         padding:30
+    },
+    headerView:{
+        flexDirection:"row"
+    },
+    imageView:{
+        width:100,
+        height:100,
+        borderRadius:100,
+        marginBottom:5,
+        elevation:4
+    },
+    image:{
+        width:100,
+        height:100,
+        borderRadius:100,
+        marginBottom:5
+    },
+    name:{
+        fontSize: 28, fontWeight: "bold" 
+    },
+    noProfile:{
+        backgroundColor:'#fff',
+        width:360,
+        borderRadius: 20,
+        marginBottom:10,
+        alignSelf:'center',
+        padding:90,
+        textAlign:'center'
+    }
+});
