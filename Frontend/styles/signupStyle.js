18413b3db81392c03dc5db2a60e9c40e051c2c2b
Caline <calineyammine123@gmail.com>
2022-04-06 23:02:39 +0300

    Created a style sheet for sign up page

---
+++ Frontend/styles/signupStyle.js
@@ -0,0 +1,18 @@
+import { StyleSheet} from 'react-native';
+
+export const signupStyles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        borderRadius:10,
+        elevation:2,
+        marginBottom:40,
+        width:344,
+        marginLeft:'auto',
+        marginRight:'auto',
+    },
+    map: {
+      width:325,
+      height:320,
+    }
+});
